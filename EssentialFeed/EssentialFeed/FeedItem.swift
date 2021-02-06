46246c031ebd086f30e25504e831ff9365d2c262
Elkana Orbach <elkana.orbach@au10tix.com>
2021-02-06 23:41:08 +0200

    Create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Elkana Orbach on 06/02/2021.
+//
+
+import Foundation
+
+struct FeedItem {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let imageURL: URL
+}
