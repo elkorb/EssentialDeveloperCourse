07711318d34add73ba95bba3a175c52b352b9144
Elkana Orbach <elkorb@gmail.com>
2021-04-01 01:16:00 +0300

    Create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Elkana Orbach on 01/04/2021.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
