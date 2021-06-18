d48fc1de418bc6c9284c784a4116a38a103a1d50
Elkana Orbach <eorbach@perforce.com>
2021-06-18 18:32:04 +0300

    Save command requests cache deletion

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,14 +6,25 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class LocalFeedLoader {
+    private let store: FeedStore
     init(store: FeedStore) {
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCacheFeed()
     }
 }
 
 class FeedStore {
     var deleteCacheFeedCallCount = 0
+    
+    func deleteCacheFeed() {
+        deleteCacheFeedCallCount += 1
+    }
 }
 
 class CacheFeeduseCaseTests: XCTestCase {
@@ -23,5 +34,20 @@ class CacheFeeduseCaseTests: XCTestCase {
         _ = LocalFeedLoader(store:store)
         
         XCTAssertEqual(store.deleteCacheFeedCallCount, 0)
+    }
+    func test_save_requestCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store:store)
+        let items = [uniqueItem(),uniqueItem()]
+        sut.save(items)
+        XCTAssertEqual(store.deleteCacheFeedCallCount, 1)
+    }
+    
+    //MARK: - Helpers
+    private func uniqueItem() -> FeedItem {
+        FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+    private func anyURL() -> URL {
+        URL(string: "https://any-url.com")!
     }
 }
