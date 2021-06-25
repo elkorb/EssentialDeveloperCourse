a0ea74bff60d9566e5a3df6122acfc44625f6221
Elkana Orbach <eorbach@perforce.com>
2021-06-25 11:38:33 +0300

    Move `FeedStore` to its own file

---
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Elkana Orbach on 25/06/2021.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?)-> Void
+    typealias InsertionCompletion = (Error?)-> Void
+
+    func deleteCacheFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items:[FeedItem],timestamp:Date, completion: @escaping InsertionCompletion)
+}
