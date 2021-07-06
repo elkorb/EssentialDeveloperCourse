b661167d3908618d1be0f23cba7579ce3a39cec6
Elkana Orbach <eorbach@perforce.com>
2021-07-06 15:45:08 +0300

    Retrieving from non-empt cache twice delivers same found result (no side-effect)

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -130,27 +130,57 @@ class CodableFeedStoreTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
-    // - MARK: Helpers
-    
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
-        let sut = CodableFeedStore(storeURL: testSpecificStoreURL())
-        trackForMemoryLeaks(sut,file: file,line: line)
-        return sut
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        let sut = makeSUT()
+        let feed = uniqueImageFeed().local
+        let timestamp = Date()
+        let exp = expectation(description: "Wait for cache retrieval")
+        
+        sut.insert(feed,timestamp: timestamp) { insertionError in
+            XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
+            
+            sut.retrieve { firstResult in
+                sut.retrieve { secondResult in
+                    switch (firstResult,secondResult) {
+                    case let (.found(expectedFeed,expectedTimestamp), .found(retrievedFeed, retrievedTimestamp)):
+                        
+                        XCTAssertEqual(expectedFeed, feed)
+                        XCTAssertEqual(expectedTimestamp, timestamp)
+                        
+                        XCTAssertEqual(retrievedFeed, feed)
+                        XCTAssertEqual(retrievedTimestamp, timestamp)
+                        
+                    default:
+                        XCTFail("Expected retrieving twice from non empty cache to deliver same found result with feed \(feed) and \(timestamp), got \(firstResult) and \(secondResult) instead.")
+                    }
+                    exp.fulfill()
+                }
+            }
+        }
+        wait(for: [exp], timeout: 1.0)
     }
-    
-    private func setupEmptyStoreState() {
-        deleteStoreArtifacts()
+
+// - MARK: Helpers
+        
+        private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
+            let sut = CodableFeedStore(storeURL: testSpecificStoreURL())
+            trackForMemoryLeaks(sut,file: file,line: line)
+            return sut
+        }
+        
+        private func setupEmptyStoreState() {
+            deleteStoreArtifacts()
+        }
+        
+        private func undoStoreSideEffects() {
+            deleteStoreArtifacts()
+        }
+        
+        private func deleteStoreArtifacts() -> ()? {
+            try? FileManager.default.removeItem(at: testSpecificStoreURL())
+        }
+        
+        private func testSpecificStoreURL() -> URL {
+            return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self)).store")
+        }
     }
-    
-    private func undoStoreSideEffects() {
-        deleteStoreArtifacts()
-    }
-    
-    private func deleteStoreArtifacts() -> ()? {
-        try? FileManager.default.removeItem(at: testSpecificStoreURL())
-    }
-    
-    private func testSpecificStoreURL() -> URL {
-        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self)).store")
-    }
-}
